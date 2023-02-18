e4d19e55c7fe80600e3c11b6359f09aaa078d6b6
aeshorotich <aeshorotich@gmail.com>
2023-02-18 14:57:59 -0800

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+	char letter = 'a';
+
+	while (num < 10)
+	{
+		putchar(num + '0');
+		num++;
+	}
+	while (letter <= 'f')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	
+	return (0);
+}
