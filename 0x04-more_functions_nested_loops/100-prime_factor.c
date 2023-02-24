df517de187e4116cbe2aaf19fdd41748eed258f6
aeshorotich <aeshorotich@gmail.com>
2023-02-23 23:35:12 -0800

    11. The problem of distinguishing prime numbers from composite numbers and of resolving the latter into their prime factors is known to be one of the most important and useful in arithmetic

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,42 +1,27 @@
-#include "main.h"
 #include <stdio.h>
+#include <math.h>
 
 /**
- *main - Prints a Fizz Buzz program
+ *main - Prints the largest prime factor of the number 612852475143
  *Return: Always 0 (Success)
  */
 
 int main(void)
 
 {
-	int x;
+	long x, maxf;
+	long number = 612852475143;
+	double square = sqrt(number);
 
-	for (x = 1; x <= 100; x++)
+	for (x = 1; x <= square; x++)
 	{
-		if ((x % 3 == 0) && (x % 5 == 0))
+		if (number % x == 0)
 		{
-			printf("FizzBuzz");
-		}
-		else if (x % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if (x % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else
-		{
-			printf("%d", x);
-		}
-
-		if (x != 100)
-		{
-			printf(" ");
+			maxf = number / x;
 		}
 	}
 
-	printf("\n");
+	printf("%ld\n", maxf);
 	return (0);
 
 }
